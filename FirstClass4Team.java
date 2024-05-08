b10b4e8c9a7f919ccba3c97f7ab5090c644df52b
Ald01110011 <01157150@email.ntou.edu.tw>
2024-05-08 15:43:17 +0800

    老二

--- FirstClass4Team.java
+++ FirstClass4Team.java
@@ -2,5 +2,6 @@ public class FirstClass4Team {
     public static void main(String[] args) {
         System.out.println("這是用來練習GitHub協作的程式!");
         System.out.println("\"學號：\"01157135\"\n姓名：王宜安\"\"");
+        System.out.println("\"學號：\"01157150\"\n姓名：楊皓鈞\"\"");
     }
 }
